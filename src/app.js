c21b993f67dc49518515f241142471b1e059779d
linchengdong <linchengdong@ins24.com>
2020-01-14 14:47:55 +0800

    feat: 项目分层

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: 服务启动文件
+ * @Author: chengDong
+ * @Date: 2020-01-14 14:39:49
+ * @LastEditTime : 2020-01-14 14:47:17
+ * @LastEditors  : chengDong
+ */
+const Koa = require('koa')
+const app = new Koa()
+
+
+
+
